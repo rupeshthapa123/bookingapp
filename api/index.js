511d5640ca8319260a2e962844d6003257ab59c1
Rupesh <rupesh.thapa2050@gmail.com>
2022-06-11 10:37:44 +0545

    first commit

---
+++ api/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const authRoute  = require('./routes/auth');
+const usersRoute  = require('./routes/users');
+const hotelsRoute  = require('./routes/hotels');
+const roomsRoute  = require('./routes/rooms');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+dotenv.config();
+
+const connect = async () =>{
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to mongoDB.");
+    }catch (error){
+        throw error;
+    }
+};
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log("mongoDB disconnected!");
+})
+
+mongoose.connection.on("connected", ()=>{
+    console.log("mongoDB connected!");
+})
+
+//middleware
+app.use(cookieParser())
+app.use(express.json())
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.use((err, req, res, next)=>{
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!";
+    return res.status(errorStatus).json({
+        success:false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack
+    });
+});
+
+app.listen(8800, ()=>{
+    connect()
+    console.log("Connected on PORT.");
+});
---
+++ api/routes/hotels.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const {createHotel, updateHotel, 
+    deleteHotel, getHotel, 
+    getHotels} = require("../controllers/hotel");
+const { verifyAdmin } = require("../utils/verifyToken");
+
+const router = express.Router();
+
+//CREATE
+router.post("/",verifyAdmin, createHotel);
+//UPDATE
+router.put("/:id",verifyAdmin, updateHotel);
+//DELETE
+router.delete("/:id",verifyAdmin, deleteHotel);
+//GET
+router.get("/:id",getHotel);
+//GETALL
+router.get("/", getHotels);
+
+module.exports = router;
